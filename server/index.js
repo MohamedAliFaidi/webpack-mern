9d7e009c6b4a67be6448b4a8d4890e0f6e85eb5f
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-18 13:43:48 +0100

    setup vite react and express in the same bundle using webpack and express static

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+
+// Your other middleware and routes here
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
