fc6ba8cd7518208f1dbba00d179c998eb593c78d
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-18 14:13:17 +0100

    test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,22 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard({action: 'deny'}))
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
+app.use(helmet.hsts({maxAge:90*24*60*60 , force: true}))
+app.use(helmet.dnsPrefetchControl())
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+    },
+  })
+);
 
 // Your other middleware and routes here
 
