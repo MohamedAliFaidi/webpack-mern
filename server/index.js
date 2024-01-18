942c199c4fe1a275c85c4b96b443b32430c950c6
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-18 14:00:42 +0100

    proxy setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,9 +13,14 @@ app.use(helmet());
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Catch-all route to serve the React app
+app.get('/v1', (req, res) => {
+  res.status(200).json({message:"ok"})
+});
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
