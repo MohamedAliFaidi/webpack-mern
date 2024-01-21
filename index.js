686ca2ab1fbb6e237eb2e26cd4fa660757361fa9
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-21 19:50:32 +0100

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.options('*', cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE',"Access-Control-Allow-Origin");
   // Add other headers as needed
   next();
 });
