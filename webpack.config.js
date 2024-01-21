c4627f95e236313f88c5db3ee621d3fc3c471c41
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-21 20:41:05 +0100

    test

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CompressionPlugin = require("compression-webpack-plugin");
+module.exports = {
+  target: 'node', // Specify the target environment (node.js)
+  entry: './index.js', // Entry point for your server
+  mode:"production", //
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+
+  output: {
+    filename: 'server.bundle.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new CompressionPlugin()],
